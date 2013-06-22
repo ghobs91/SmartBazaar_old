fc614a725a43a77dafb57c6bc04d31032f4269f9
nmallinar <nmallinar@gmail.com>
2013-06-21 17:17:02 -0700

    i think i fixed my db relationships...

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :condition, :price, :shipping_cost, :description, :post_date, :seller_id
 
   belongs_to :seller, class_name: "User"
+  has_one :transaction
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,5 @@ class Transaction < ActiveRecord::Base
 	validates_presence_of :buyer_id, :product_id, :purchase_date
 
 	belongs_to :buyer, class_name: "User"
+	belongs_to :product
 end

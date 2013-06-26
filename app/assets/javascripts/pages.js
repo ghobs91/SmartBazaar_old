89d36396e1dea99125f45bad0b2e8e8a5f4bda1e
nmallinar <nmallinar@gmail.com>
2013-06-25 18:07:35 -0700

    html fixes removed redundant code and files, animated how it works

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -4,6 +4,7 @@
 
 $(document).ready(function(){
 	home.init();
+	how.init();
 
 	$('.alert').fadeOut(5000);
 	$('.notice').fadeOut(5000);
@@ -46,6 +47,20 @@ home = {
 			parent.children('.arrow-up').animate({ marginLeft: '515px' }, 600);
 			parent.find('#home-page-search').attr('data-search', 'browse');
 			parent.find('#home-page-search-form').attr('action', 'browse');
+		});
+	}
+}
+
+how = {
+	init: function(){
+		$("#how-seller").css("display","none");
+		$("#how-buy-btn").bind("click", function(){
+			$("#how-seller").slideUp("fast");
+			$("#how-buyer").slideDown("fast");
+		});
+		$("#how-sell-btn").bind("click", function(){
+			$("#how-buyer").slideUp("fast");
+			$("#how-seller").slideDown("fast");
 		});
 	}
 }
